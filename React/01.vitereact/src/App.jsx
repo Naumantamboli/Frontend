d33bdb7d17ee451ac2e56569935dd7cfa4c23a41
Nauman Tamboli <tambolinauman7@gmail.com>
2024-10-08 17:57:54 +0530

    changes made

---
+++ React/01.vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Nauman from "./nauman"
+
+function App() {
+  return (
+    <>
+    <h1>Nauman Tamboli with vite</h1>   
+    <Nauman /> 
+    </>
+  )
+}
+
+export default App
---
+++ React/01.vitereact/src/nauman.jsx
@@ -0,0 +1,12 @@
+//this is like a component 
+
+function Nauman(){
+    return(
+        <>
+            <h2>My name is nauman</h2>
+            <i>I am in 3rd year</i>
+        </>
+    )
+}
+
+export default Nauman
