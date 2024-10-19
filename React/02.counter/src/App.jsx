c422852128f4aa1d322193c207aca517192d7548
Nauman Tamboli <tambolinauman7@gmail.com>
2024-10-19 11:05:11 +0530

    changes made

--- React/02.counter/src/App.jsx
+++ React/02.counter/src/App.jsx
@@ -1,18 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  let [counter,setCounter] = useState(10)   //hook
+
+  // let counter = 5;
+
+  const addValue = () =>{
+    if(counter<20)
+    setCounter(counter+1);
+  }
+
+  const decValue = () =>{
+    if(counter>0)
+    setCounter(counter-1);
+  }
+
   return (
     <>
       <h1>Chai aur react</h1>
-      <h2>Counter value</h2>
+      <h2>Counter value: {counter}</h2>
 
-      <button>Add value</button>
+      <button
+      onClick={addValue}
+      >Add value {counter}</button>
       <br />
-      <button>Decrease value</button>
+      <button
+      onClick={decValue}
+      >Decrease value {counter}</button>
     </>
   )
 }
